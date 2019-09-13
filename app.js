2cb56e89062a61f48f4a085cbe34734c7e43926e
vs4vijay <vs4vijay@gmail.com>
2019-09-13 16:20:58 +0530

    Add Encrypt and Decryp API

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2');
-const express = require('express');
+const express = require('express'); 
 
 const app = express();
 
@@ -8,6 +8,9 @@ const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_NAME = process.env.DB_NAME || 'viz_db';
 const DB_USERNAME = process.env.DB_USERNAME || 'viz1';
 const DB_PASSWORD = process.env.DB_PASSWORD || '123456';
+const ENCRYPT_KEY = process.env.AES_KEY || '';
+
+// app.use(express.json());
 
 app.use(function(req, res, next) {
 	res.locals.connection = mysql.createConnection({
@@ -20,17 +23,70 @@ app.use(function(req, res, next) {
 	next();
 });
 
-app.get('/api/v1/health-check', function(req, res, next) {
+app.get(['/', '/api/v1/health-check'], function(req, res, next) {
 	res.locals.connection.query('SELECT 1+1 AS sum', function (error, results, fields) {
-        if (error) throw error;
+        if(error) {
+			console.error('error', error);
+			return next(error);
+		}
         
-        data = {
+        response = {
             success: results[0]['sum'] == 2,
             data: results
         }
-		res.send(data);
+		res.json(response);
 	});
 });
+
+app.get('/api/v1/encrypt', (req, res, next) => {
+	const encryptSqlQuery = `SELECT HEX(AES_ENCRYPT("?", "${ENCRYPT_KEY}")) AS encrypted_data LIMIT 1`;
+
+	const inputData = req.query['data'];
+
+	res.locals.connection.query(encryptSqlQuery, [inputData], function (error, results, fields) {
+        if(error) {
+			console.error('error', error);
+			return next(error);
+		}
+        
+        response = {
+            success: true,
+            data: results[0]
+        }
+		res.json(response);
+	});
+});
+
+app.get('/api/v1/decrypt', (req, res, next) => {
+	const decryptSqlQuery = `SELECT AES_DECRYPT(UNHEX("?"), "${ENCRYPT_KEY}") AS decrypted_data LIMIT 1`;
+
+	const inputData = req.query['data'];
+
+	console.log('inputData', inputData);
+
+	res.locals.connection.query(decryptSqlQuery, [inputData], function (error, results, fields) {
+        if(error) {
+			console.error('error', error);
+			return next(error);
+		}
+        
+        response = {
+            success: true,
+            data: results[0]
+        }
+		res.json(response);
+	});
+});
+
+app.use(function (err, req, res, next) {
+	console.error(err.stack);
+
+	response = {
+		success: false,
+		errors: err.stack
+	};
+	res.status(500).json(response);
+})
 
 app.listen(APP_PORT, _ => {
     console.log(`[+] App Server started on ${APP_PORT}`);
